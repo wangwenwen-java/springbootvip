e260f7355d0cc3afe6e0b990d876c70dbcd68281
wangwenwen-java <393699844@qq.com>
2020-03-29 21:12:20 +0800

    v0.2

--- src/main/java/cn/enjoy/controller/UserController.java
+++ src/main/java/cn/enjoy/controller/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
 
     @RequestMapping("/hello")
     public Object home() {
-        logger.debug("这是个hello的日志");
+        logger.debug("这是个hello的日志----wang");
         return "Hello World!99";
     }
 
